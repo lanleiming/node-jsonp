88ddc86c43b76f0574b39d8f7fdeee77ff3c28ee
lanleiming <lanleiming@360.cn>
2017-06-22 15:54:23 +0800

    完善！

--- hello.js
+++ hello.js
@@ -1,16 +1,68 @@
 /**
  * node.js的入口文件.
+ * 必须的参数：file - 文件名称； callback - jsonp形式调用
+ * 返回jsonp形式
  */
-var http = require("http");
+var http = require('http');
+var url=require('url');
+var querystring = require('querystring');
+var fs=require('fs');
+var path=require('path');
+
+function writeMsg(response,msg){
+    response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8;'});
+    response.write(msg);
+    response.end();
+}
+
+
 http.createServer(function(request, response){
-    //发送http头部ͷ
-    //http状态值：200
-    //内容类型：text/html
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    //设置显示字符编码
-    response.write("<head><meta charset='utf-8'></head>");
-    //发送响应数据
-    response.end("Hello World! 来自node.js1 \n");
-    //后台终端打印信息
-    console.log("Server running at http://localhost:3000/hello world!");
-}).listen(3000);
+    //获取url的query
+    var arg = url.parse(request.url).query;
+    //转换为json形式
+    var arg_json = querystring.parse(arg);
+    //输出的内容
+    var content = '';
+    var filepath = arg_json.file;
+    var callback =arg_json.callback;
+
+    if(filepath){
+       //file -- 本地的json文件路径
+        filepath = path.resolve('./json/'+filepath);
+       //判断文件是否存在
+       if(!fs.existsSync(filepath)){
+            content='error:文件不存在';
+            writeMsg(response,content);
+       }else{
+           fs.readFile(filepath,'utf-8',function(err,data){
+               if(err){
+                   console.log(err);
+                   content='error:文件读取失败';
+                   writeMsg(response,content);
+               }else{
+                   //读取成功
+                   if(callback){
+                       //callback -- jsonp形式
+                       response.writeHead(200, {'Content-Type': 'application/javascript;charset=utf-8;'});
+                       content = callback+'('+data+');';
+                       response.write(content);
+                       response.end();
+                       return false;
+                   }
+                   else{
+                       //callback -- jsonp形式
+                       response.writeHead(200, {'Content-Type': 'text/json;charset=utf-8;'});
+                       response.write(data);
+                       response.end();
+                       return false;
+                   }
+               }
+           });
+       }
+    }else{
+        content='error:参数错误';
+        writeMsg(response,content);
+    }
+
+
+}).listen(3001);
